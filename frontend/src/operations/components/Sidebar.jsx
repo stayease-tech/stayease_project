b086a566dadeba76352dcad7d534e66504d407db
Jibin Roy <jibin@mystayease.com>
2026-07-30 13:20:12 +0530

    Operations Sidebar Changes

--- frontend/src/operations/components/Sidebar.jsx
+++ frontend/src/operations/components/Sidebar.jsx
@@ -1,23 +1,43 @@
 // Copyright Aravind Adari
-import { LiaBedSolid } from "react-icons/lia";
-import { VscChecklistCompact } from "react-icons/vsc";
-import { GoChecklist } from "react-icons/go";
-import { GiTicket } from "react-icons/gi";
-import { FaFileContract } from "react-icons/fa";
-import { MdMoveToInbox, MdOutlineExitToApp } from "react-icons/md";
-import { TbReportMoney } from "react-icons/tb";
-import SharedSidebar from "../../shared/Sidebar";
+import { LiaBedSolid } from 'react-icons/lia';
+import { VscChecklist } from 'react-icons/vsc';
+import { GoChecklist } from 'react-icons/go';
+import { GiTicket } from 'react-icons/gi';
+import { FaFileContract } from 'react-icons/fa';
+import { MdMoveToInbox, MdOutlineExitToApp } from 'react-icons/md';
+import { TbReportMoney } from 'react-icons/tb';
+import SharedSidebar from '../../shared/Sidebar';
 
 const menuItems = [
-    { name: "View Beds", icon: <LiaBedSolid />, link: "/operations/operations-beds-table" },
-    { name: "Checklist & Feedback", icon: <VscChecklistCompact />, link: "/operations/operations-checklistfeedback-table" },
-    { name: "Complaints", icon: <GiTicket />, link: "/operations/operations-propertycomplaint-table" },
-    { name: "KYC Management", icon: <GoChecklist />, link: "/operations/operations-kyc-management" },
-    { name: "Expenses", icon: <TbReportMoney />, link: "/operations/operations-expense-table" },
-    // { name: "Move-In", icon: <MdMoveToInbox />, link: "/operations/operations-checklistfeedback-table?type=movein" },
-    // { name: "Move-Out", icon: <MdOutlineExitToApp />, link: "/operations/operations-checklistfeedback-table?type=moveout" },
+  {
+    name: 'View Beds',
+    icon: <LiaBedSolid />,
+    link: '/operations/operations-beds-table',
+  },
+  {
+    name: 'Checklist & Feedback',
+    icon: <VscChecklist />,
+    link: '/operations/operations-checklistfeedback-table',
+  },
+  {
+    name: 'Complaints',
+    icon: <GiTicket />,
+    link: '/operations/operations-propertycomplaint-table',
+  },
+  {
+    name: 'KYC Management',
+    icon: <GoChecklist />,
+    link: '/operations/operations-kyc-management',
+  },
+  {
+    name: 'Expenses',
+    icon: <TbReportMoney />,
+    link: '/operations/operations-expense-table',
+  },
+  // { name: "Move-In", icon: <MdMoveToInbox />, link: "/operations/operations-checklistfeedback-table?type=movein" },
+  // { name: "Move-Out", icon: <MdOutlineExitToApp />, link: "/operations/operations-checklistfeedback-table?type=moveout" },
 ];
 
 export default function Sidebar() {
-    return <SharedSidebar menuItems={menuItems} />;
+  return <SharedSidebar menuItems={menuItems} />;
 }
